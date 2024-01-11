9fbaf991e5f50ccc4cc0e44ca3eef9a56d0ac668
Ellnamin <ellnamin.business@gmail.com>
2024-01-11 23:00:01 +0330

    feat: complete project

--- project.py
+++ project.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from api import verification_code
+import os
 
 
 def register_bot():
@@ -13,14 +14,15 @@ def register_bot():
 
     try:
         # Navigate to the online exchange website
-        driver.get("http://yans-ex.ir/")
+        current_directory = os.getcwd()
+        driver.get(f"file://{current_directory}/index.html")
 
         # Check if the registration is open by finding the element that indicates it
         try:
             start_button = WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable(
-                    (By.ID, "start-button")
-                )  # Replace with the actual ID or other locator
+                    (By.XPATH, "//button[contains(text(), 'نوبت دهی')]")
+                )
             )
         except:
             print("Registration not open or element not found.")
@@ -29,6 +31,7 @@ def register_bot():
         # Check and accept the policies
         if check_and_accept_policies(driver):
             # Click the start button
+            time.sleep(1)
             start_button.click()
 
             # Enter phone number and handle verification
@@ -45,7 +48,7 @@ def check_and_accept_policies(driver):
     try:
         checkbox = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable(
-                (By.ID, "policy-checkbox")
+                (By.ID, "acceptRules")
             )  # Replace with the actual ID or other locator
         )
         checkbox.click()
@@ -60,41 +63,38 @@ def enter_phone_number(driver):
     # For example, find the input field, enter the number, and click verify
     try:
         phone_input = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable(
-                (By.ID, "input-30")
-            )
+            EC.element_to_be_clickable((By.ID, "input-30"))
         )
         phone_input.send_keys("1234567890")  # Replace with the actual phone number
 
         continue_button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable(
-                (By.XPATH, "//button[contains(text(), 'ادامه')]")
-            )
+            EC.element_to_be_clickable((By.XPATH, "//input[@type='submit']"))
         )
+        time.sleep(1)
         continue_button.click()
 
+        global verification_code
         while not verification_code:
-            time.sleep(1)
+            time.sleep(5)
+            verification_code = 1235
 
         # Now, send the verification code to your Flask API endpoint
         verify_input = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable(
-                (By.ID, "input-35")
-            )
+            EC.element_to_be_clickable((By.NAME, "verificationCode"))
         )
-        verify_input.send_keys(
-            verification_code
-        )
+        verify_input.send_keys(verification_code)
 
         verify_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable(
-                (By.XPATH, "//button[contains(text(), 'تایید')]")
+                (By.XPATH, "//input[@type='submit']")
             )  # Replace with the actual ID or other locator
         )
+        time.sleep(1)
         verify_button.click()
 
     except:
         print("Unable to enter phone number or verify.")
+
 
 # Schedule the bot to run every day at 8 p.m.
 schedule.every().day.at("20:00").do(register_bot)
